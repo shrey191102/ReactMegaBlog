030227f886fb604a089531014cd7cb6125d5b39e
shrey191102 <shreyanshsingh0019@gmail.com>
2024-10-06 12:26:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  // console.log(process.env.REACT_APP_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1>helloworkd </h1>
+        
+    </>
+  )
+}
+
+export default App
