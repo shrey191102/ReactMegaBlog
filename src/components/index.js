d4d9f7640cbd2485272a7254be91488c5690816d
shrey191102 <shreyanshsingh0019@gmail.com>
2024-11-13 22:10:19 +0530

    added more components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "../components/container/Container";
+import Logo from "../components/Logo";
+import LogoutBtn from "./header/LogoutBtn";
 
 export {
     Header,
     Footer,
+    Container,
+    Logo,
+    LogoutBtn
     // Add more components here as needed...
 }
